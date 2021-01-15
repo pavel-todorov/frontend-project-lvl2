b8db050fa7ffc6daae27f2ef1472ec90039f88dc
pavel.todorov <pavel.todorov@hiq.ru>
2021-01-15 15:53:15 +0300

    Init and add files.

---
+++ gendiff.js
@@ -0,0 +1,3 @@
+export default function getDiff(path1, path2) {
+  //@todo
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+const { program } = require('commander');
+
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-h, --help', 'output usage information')
+  .parse(process.argv);
+
+const options = program.opts();
+if (options.help === true) {
+  program.outputHelp();
+}
