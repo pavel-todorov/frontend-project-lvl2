794c5d479448dd3d94a0dfac2edff9d958b2f72c
pavel.todorov <pavel.todorov@hiq.ru>
2021-01-15 16:11:00 +0300

    Add desctions to help option. Add arguments.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const { program } = require('commander');
 
 program
+  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
   .option('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
 
 const options = program.opts();
