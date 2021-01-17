6f660650591f5c9b0daf03faf00a49efe6ecadaa
pavel.todorov <pavel.todorov@hiq.ru>
2021-01-17 18:11:38 +0300

    Add arguments to gendiff command.

--- gendiff.js
+++ gendiff.js
@@ -1,3 +1,5 @@
-export default function getDiff(path1, path2) {
+module.exports = async function (path1, path2) { 
+  console.log(`Source:\n${path1}\n${path2}`);
   //@todo
-}
+  return 'Not implemented';
+};
--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
 const { program } = require('commander');
+const getDiff = require('./gendiff.js');
 
 program
+  .arguments('[filepath1] [filepath2]')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .arguments('<filepath1> <filepath2>')
   .option('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
-  .parse(process.argv);
+  .action(async (file1, file2) => {
+    const result = await getDiff(file1, file2);
+    console.log(`Result:\n${result}`);
+  });
+
+program.parse(process.argv);
 
 const options = program.opts();
 if (options.help === true) {
   program.outputHelp();
 }
+
+
+
+
+
+
