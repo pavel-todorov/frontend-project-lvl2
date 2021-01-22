dfa438c91e4e2106ddc3b970804d9c63f40fc5bf
pavel.todorov <pavel.todorov@hiq.ru>
2021-01-22 11:01:02 +0300

    Fix codeclimate issue.

--- gendiff.js
+++ gendiff.js
@@ -25,6 +25,8 @@ const getObjectsFields = (src, path = '') => {
 
 const isObjectAndNotNull = (src) => (typeof src === 'object' && src !== null);
 
+const isNotObjectAndNotNull = (src) => !isObjectAndNotNull(src);
+
 const isNotSpecialCase = (caseValue, field) => (caseValue === undefined
   || !lodash.startsWith(field, caseValue));
 
@@ -45,73 +47,109 @@ const computeSpecialCase = (caseValue, field) => {
   return caseValue;
 };
 
+const isUndefined = (value) => value === undefined;
+
+const checkMatrix = [
+  {
+    check: [isObjectAndNotNull, isObjectAndNotNull],
+    do: (resultArray, specialCases, field) => {
+      resultArray.push(new ResultItem(' ', field));
+    },
+  },
+  {
+    check: [isUndefined, isObjectAndNotNull],
+    do: (resultArray, specialCases, field) => {
+      resultArray.push(checkIsNotSpecialCaseAndGenerateItem(specialCases.checkGroup.get('+'), field, '+'));
+      if (isNeedToAddSpecialCase(specialCases.checkGroup.get('+'), field)) {
+        specialCases.checkGroup.set('+', field);
+      }
+    },
+  },
+  {
+    check: [isObjectAndNotNull, isUndefined],
+    do: (resultArray, specialCases, field) => {
+      resultArray.push(checkIsNotSpecialCaseAndGenerateItem(specialCases.checkGroup.get('-'), field, '-'));
+      specialCases.checkGroup.set('-', computeSpecialCase(specialCases.checkGroup.get('-'), field));
+    },
+  },
+  {
+    check: [isObjectAndNotNull, isNotObjectAndNotNull],
+    do: (resultArray, specialCases, field, value1, value2) => {
+      resultArray.push(new ResultItem('-', field));
+      specialCases.checkGroup.set('-', computeSpecialCase(specialCases.checkGroup.get('-'), field));
+      specialCases.lastInGroup.set(field, new ResultItem('+', field, value2));
+    },
+  },
+  {
+    check: [isNotObjectAndNotNull, isObjectAndNotNull],
+    do: (resultArray, specialCases, field, value1) => {
+      resultArray.push(new ResultItem('-', field, value1));
+      resultArray.push(new ResultItem('+', field));
+    },
+  },
+  {
+    check: [isNotObjectAndNotNull, isUndefined],
+    do: (resultArray, specialCases, field, value1) => {
+      resultArray.push(checkIsNotSpecialCaseAndGenerateItem(specialCases.checkGroup.get('-'), field, '-', value1));
+    },
+  },
+  {
+    check: [isUndefined, isNotObjectAndNotNull],
+    do: (resultArray, specialCases, field, value1, value2) => {
+      resultArray.push(checkIsNotSpecialCaseAndGenerateItem(specialCases.checkGroup.get('+'), field, '+', value2));
+    },
+  },
+  {
+    check: [isNotObjectAndNotNull, isNotObjectAndNotNull],
+    do: (resultArray, specialCases, field, value1, value2) => {
+      if (value1 === value2) {
+        resultArray.push(new ResultItem(' ', field, value1));
+      } else {
+        resultArray.push(new ResultItem('-', field, value1));
+        resultArray.push(new ResultItem('+', field, value2));
+      }
+    },
+  },
+];
+
 const compareObjectsByFields = (object1, object2, fields) => {
   const resultArray = new ResultArray();
   const specialCases = {
     lastInGroup: new Map(),
-    checkGroupAdded: undefined,
-    checkGroupRemoved: undefined,
+    checkGroup: new Map(),
   };
   fields.forEach((field) => {
     if (lodash.endsWith(field, '<<<<<')) {
       resultArray.push(new ResultItem('<', field));
-      if (!lodash.startsWith(field, specialCases.checkGroupAdded)) {
-        specialCases.checkGroupAdded = undefined;
+      if (!lodash.startsWith(field, specialCases.checkGroup.get('+'))) {
+        specialCases.checkGroup.set('+', undefined);
       }
-      if (!lodash.startsWith(field, specialCases.checkGroupRemoved)) {
-        specialCases.checkGroupRemoved = undefined;
+      if (!lodash.startsWith(field, specialCases.checkGroup.get('-'))) {
+        specialCases.checkGroup.set('-', undefined);
       }
     } else {
       const value1 = lodash.get(object1, field);
       const value2 = lodash.get(object2, field);
-      if (isObjectAndNotNull(value1) && isObjectAndNotNull(value2)) {
-        resultArray.push(new ResultItem(' ', field));
-      } else if (value1 === undefined && isObjectAndNotNull(value2)) {
-        resultArray.push(checkIsNotSpecialCaseAndGenerateItem(specialCases.checkGroupAdded, field, '+'));
-        if (isNeedToAddSpecialCase(specialCases.checkGroupAdded, field)) {
-          specialCases.checkGroupAdded = field;
-        }
-      } else if (isObjectAndNotNull(value1) && value2 === undefined) {
-        resultArray.push(checkIsNotSpecialCaseAndGenerateItem(specialCases.checkGroupRemoved, field, '-'));
-        specialCases.checkGroupRemoved = computeSpecialCase(specialCases.checkGroupRemoved, field);
-      } else if (isObjectAndNotNull(value1) && !isObjectAndNotNull(value2)) {
-        resultArray.push(new ResultItem('-', field));
-        specialCases.checkGroupRemoved = computeSpecialCase(specialCases.checkGroupRemoved, field);
-        specialCases.lastInGroup.set(field, new ResultItem('+', field, value2));
-      } else if (!isObjectAndNotNull(value1) && isObjectAndNotNull(value2)) {
-        resultArray.push(new ResultItem('-', field, value1));
-        resultArray.push(new ResultItem('+', field));
-      } else if (!isObjectAndNotNull(value1) && value2 === undefined) {
-        // console.log(`CheckRemoved: ${specialCases.checkGroupRemoved}`);
-        resultArray.push(checkIsNotSpecialCaseAndGenerateItem(specialCases.checkGroupRemoved, field, '-', value1));
-      } else if (value1 === undefined && !isObjectAndNotNull(value2)) {
-        // console.log(`CheckAdded: ${specialCases.checkGroupAdded}`);
-        resultArray.push(checkIsNotSpecialCaseAndGenerateItem(specialCases.checkGroupAdded, field, '+', value2));
-      } else if (!isObjectAndNotNull(value1) && !isObjectAndNotNull(value2)) {
-        if (value1 === value2) {
-          resultArray.push(new ResultItem(' ', field, value1));
-        } else {
-          resultArray.push(new ResultItem('-', field, value1));
-          resultArray.push(new ResultItem('+', field, value2));
+
+      for (let i = 0; i < checkMatrix.length; i += 1) {
+        const check = checkMatrix[i];
+        if (check.check[0](value1) && check.check[1](value2)) {
+          check.do(resultArray, specialCases, field, value1, value2);
+          break;
         }
       }
-      // console.log(`Result: ${resultArray.data.map((item) => item.toString()).join('\n')}`);
     }
   });
-  // console.log(`SpecialCases: ${JSON.stringify(specialCases)}`);
   if (specialCases.lastInGroup.size > 0) {
     specialCases.lastInGroup.forEach((toAdd, key) => {
-      // console.log(`Special case (to add): ${key} -> ${toAdd}`);
       resultArray.insertAfterClosedKey(key, toAdd);
     });
   }
-  // console.log(`>>>Result: ${resultArray.data.map((item) => item.toString()).join('\n')}`);
   return resultArray;
 };
 
 const getDiff = (path1, path2, formatter = 'stylish') => {
   try {
-    // const resultArray = new ResultArray();
     const objects = [path1, path2].map((path) => ({ path, object: getObject(path) }));
     const error = objects.find((value) => value.object === undefined);
     if (error !== undefined) {
@@ -123,9 +161,7 @@ const getDiff = (path1, path2, formatter = 'stylish') => {
 
     const fields = lodash.union(getObjectsFields(object1), getObjectsFields(object2))
       .sort(sortAsc);
-    // console.log(`Fields:\n${fields.join('\n')}`);
     const resultArray = compareObjectsByFields(object1, object2, fields);
-    // console.log(`ResultArray: ${JSON.stringify(resultArray)}`);
 
     return createFormatter(formatter).format(resultArray);
   } catch (err) {
