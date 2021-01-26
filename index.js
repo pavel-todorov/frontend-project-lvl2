d3730b459659d87ff5f612cbf2cf65a369d0b35a
pavel.todorov <pavel.todorov@hiq.ru>
2021-01-26 15:52:55 +0300

    Fix colors attribute

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format', 'stylish');
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('--colors', 'just do nothing');
 
 /* eslint no-console: 0 */
 program
   .arguments('[filepath1] [filepath2]')
   .action((file1, file2) => {
+    if (file1 === undefined || file2 === undefined) {
+      program.outputHelp();
+      return;
+    }
+    console.log(`${file1}, ${file2}`);
     const result = genDiff(file1, file2, program.opts().format);
     console.log(`Result:\n${result}`);
   });
