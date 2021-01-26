4475045f2f41a6f4bc43118ad125317c5e77afef
pavel.todorov <pavel.todorov@hiq.ru>
2021-01-26 15:47:26 +0300

    Fix index error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-const { genDiff } = require('./gendiff.js');
+const genDiff = require('./gendiff.js');
 
 program
   .allowUnknownOption()
