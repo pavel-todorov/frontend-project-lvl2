3443c05fe640976ffb3e0381743b1e85f8a569df
pavel.todorov <pavel.todorov@hiq.ru>
2021-02-02 10:21:45 +0300

    fix code climate

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -18,25 +18,20 @@ const objectToString = (offset, obj) => {
 
 const itemToString = (item) => {
   const offset = ' '.repeat(item.getKeyLevel() * 4 - 2);
+  const prefix = `${offset}${item.state} ${item.getKeyToPrint()}: {`;
+  const postfix = `${offset}  }`;
   if (item.hasSubItems()) {
     item.normalize();
-    const prefix = `${offset}${item.state} ${item.getKeyToPrint()}: {`;
     const subItems = item.subItems.map((subItem) => itemToString(subItem)).join('\n');
-    const postfix = `${offset}  }`;
     return `${prefix}\n${subItems}\n${postfix}`;
   }
   if (item.hasValue()) {
     const valType = typeof item.value;
     if (valType === 'object' && item.value !== null) {
-      const prefix = `${offset}${item.state} ${item.getKeyToPrint()}: {`;
-      const postfix = `${offset}  }`;
       const newOffset = ' '.repeat((item.getKeyLevel() + 1) * 4 - 2);
       return `${prefix}\n${objectToString(newOffset, item.value).join('\n')}\n${postfix}`;
     }
     return `${offset}${item.state} ${item.getKeyToPrint()}: ${item.value}`;
-  }
-  if (!item.isCloseBracket()) {
-    return `${offset}${item.state} ${item.getKeyToPrint()}: {`;
   }
   return `${offset}  }`;
 };
